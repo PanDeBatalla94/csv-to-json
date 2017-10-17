429abdb3434812e617059d1290712f8631baffc5
mrcm15 <mariarenee.canqui@gmail.com>
2017-10-17 19:47:58 -0400

    modificaciones varias

--- upload_csv/web-start/scripts/main.js
+++ upload_csv/web-start/scripts/main.js
@@ -1,136 +1,31 @@
-/**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-'use strict';
 
-// Initializes FriendlyChat.
-function FriendlyChat() {
-  this.checkSetup();
+function convertir_csv(){
+   //e.preventDefault();
+   console.log("entro");
+   event.preventDefault();
+   //get nombre tabla
+    nodo =  document.getElementById('nomb_tabla').value;
 
-  // Shortcuts to DOM Elements.
-  //this.messageList = document.getElementById('messages');
-  this.messageForm = document.getElementById('message-form');
-  this.messageInput = document.getElementById('message');
-  this.submitButton = document.getElementById('submit');
-  this.submitImageButton = document.getElementById('submitImage');
-  this.imageForm = document.getElementById('image-form');
-  this.mediaCapture = document.getElementById('mediaCapture');
-  this.userPic = document.getElementById('user-pic');
-  this.userName = document.getElementById('user-name');
-  this.signInButton = document.getElementById('sign-in');
-  this.signOutButton = document.getElementById('sign-out');
-  this.signInSnackbar = document.getElementById('must-signin-snackbar');
+   var messagesRef = new Firebase("https://pruebas-csv.firebaseio.com/"+nodo);
+   d3.csv("/upload_csv/web-start/json/prueba77.csv", function(data) {
+//  document.getElementById('resultado').value=data;
 
-  // Saves message on form submit.
-  this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
-  //this.signOutButton.addEventListener('click', this.signOut.bind(this));
- // this.signInButton.addEventListener('click', this.signIn.bind(this));
+   var or;
+   var aux;
+   for (var i = 0 ; i < data.length; i++) 
+   {
+    console.log(Object.keys(data[i]).length);
+        
+    for (var key in data[i]) {
 
-  // Toggle for the button.
-  var buttonTogglingHandler = this.toggleButton.bind(this);
-  this.messageInput.addEventListener('keyup', buttonTogglingHandler);
-  this.messageInput.addEventListener('change', buttonTogglingHandler);
+      data[i][key] = data[i][key].replace('{','');
+      data[i][key] = data[i][key].replace('}','');
 
-  // Events for image upload.
-  this.submitImageButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    this.mediaCapture.click();
-  }.bind(this));
- // this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+    }
+     
 
-  this.initFirebase();
+      // messagesRef.push(data[i]);    
+    }
+  });
 }
 
-// Sets up shortcuts to Firebase features and initiate firebase auth.
-FriendlyChat.prototype.initFirebase = function() {
-  // Shortcuts to Firebase SDK features.
-  this.auth = firebase.auth();
-  this.database = firebase.database();
-  this.storage = firebase.storage();
-  // Initiates Firebase auth and listen to auth state changes.
-  //this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
-};
-
-
-// Saves a new message on the Firebase DB.
-FriendlyChat.prototype.saveMessage = function(e) {
-  e.preventDefault();
-
-  this.messagesRef = this.database.ref('messages');
-  // Make sure we remove all previous listeners.
-  this.messagesRef.off();
-
-  //this.saveMessagingDeviceToken();
-  // Check that the user entered a message and is signed in.
-  if (this.messageInput.value ) {
-    var currentUser = this.auth.currentUser;
-    // Add a new message entry to the Firebase Database.
-    this.messagesRef.push({
-      name: '',
-      text: this.messageInput.value,
-      photoUrl: ''
-    }).then(function() {
-      // Clear message text field and SEND button state.
-      FriendlyChat.resetMaterialTextfield(this.messageInput);
-      this.toggleButton();
-    }.bind(this)).catch(function(error) {
-      console.error('Error writing new message to Firebase Database', error);
-    });
-  }
-};
-
-
-// Resets the given MaterialTextField.
-FriendlyChat.resetMaterialTextfield = function(element) {
-  element.value = '';
-  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
-};
-
-// Template for messages.
-FriendlyChat.MESSAGE_TEMPLATE =
-    '<div class="message-container">' +
-      '<div class="spacing"><div class="pic"></div></div>' +
-      '<div class="message"></div>' +
-      '<div class="name"></div>' +
-    '</div>';
-
-// A loading image URL.
-//FriendlyChat.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
-
-// Displays a Message in the UI.
-
-
-// Enables or disables the submit button depending on the values of the input
-// fields.
-FriendlyChat.prototype.toggleButton = function() {
-  if (this.messageInput.value) {
-    this.submitButton.removeAttribute('disabled');
-  } else {
-    this.submitButton.setAttribute('disabled', 'true');
-  }
-};
-
-// Checks that the Firebase SDK has been correctly setup and configured.
-FriendlyChat.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
-    window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions and make ' +
-        'sure you are running the codelab using `firebase serve`');
-  }
-};
-
-window.onload = function() {
-  window.friendlyChat = new FriendlyChat();
-};
