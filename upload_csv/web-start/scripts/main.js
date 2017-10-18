a97476dd59df62bb30432b1af04629ef38609771
mrcm15 <mariarenee.canqui@gmail.com>
2017-10-18 19:59:26 -0400

    modificaciones

--- upload_csv/web-start/scripts/main.js
+++ upload_csv/web-start/scripts/main.js
@@ -1,31 +1,128 @@
 
-function convertir_csv(){
-   //e.preventDefault();
-   console.log("entro");
-   event.preventDefault();
+function convertir_csv(){ //convertir csv a json y subir informacion a firebase
+
    //get nombre tabla
     nodo =  document.getElementById('nomb_tabla').value;
+    archivo =  document.getElementById('archivo').value;
+    var messagesRef = new Firebase("https://pruebas-csv.firebaseio.com/"+nodo); 
 
-   var messagesRef = new Firebase("https://pruebas-csv.firebaseio.com/"+nodo);
-   d3.csv("/upload_csv/web-start/json/prueba77.csv", function(data) {
-//  document.getElementById('resultado').value=data;
+   d3.tsv("/upload_csv/web-start/json/"+archivo, function(data) {
 
-   var or;
-   var aux;
    for (var i = 0 ; i < data.length; i++) 
    {
-    console.log(Object.keys(data[i]).length);
-        
+    //reemplazar valores   
     for (var key in data[i]) {
 
       data[i][key] = data[i][key].replace('{','');
       data[i][key] = data[i][key].replace('}','');
-
     }
-     
-
-      // messagesRef.push(data[i]);    
+       messagesRef.push(data[i]);    
     }
+
   });
 }
 
+
+function obtener_nodos(){
+
+  var html='';
+ var messagesRef = new Firebase("https://pruebas-csv.firebaseio.com/"); 
+/*var rootRef = messagesRef.database.ref();
+var urlRef = rootRef.child("user1/DAA notes/URL");
+urlRef.once("value", function(snapshot) {
+  snapshot.forEach(function(child) {
+    console.log(child.key+": "+child.val());
+  });
+});
+ */
+
+ var t = '';
+
+  /*messagesRef.on('child_added', function(snapshot) {
+
+   // all records after the last continue to invoke this function
+   console.log(snapshot.key());
+   html = html+'<div class="card"> <div class="card-header" role="tab" id="headingOne"><h5 class="mb-0">';
+   html = html+'       <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">';
+   html = html+         snapshot.key();
+   html = html+'       </a> </h5></div>';
+
+   html = html+'   <div id="collapseOne" class="collapse " role="tabpanel" aria-labelledby="headingOne">';
+   html = html+'     <div class="card-block">';
+   html = html+'        <table>';
+            
+   html = html+'       </table>';
+   html = html+'     </div>';
+   html = html+'   </div>';
+   html = html+' </div>';
+            
+          
+
+   //, snapshot.val()
+
+
+
+});
+console.log(t);*/
+
+//child('/scenes/' + projId)
+var keys = [];
+var counts = [];
+var ref = messagesRef.ref();
+messagesRef.on('child_added',function(snap) {
+  
+html='';
+html = html+'<div class="card"> <div class="card-header" role="tab" id="headingOne"><h5 class="mb-0">';
+   html = html+'       <a data-toggle="collapse" data-parent="#accordion" href="#'+snap.key()+'" aria-expanded="true" aria-controls="collapseOne">';
+   html = html+         snap.key();
+   html = html+'       </a> </h5></div>';
+
+   html = html+'   <div id="'+snap.key()+'" class="collapse " role="tabpanel" aria-labelledby="headingOne">';
+   html = html+'     <div class="card-block">';
+   html = html+'        <table>';
+  
+
+  //html = html+'<tr>';
+   
+   count = 0;
+   html_tab='';
+   snap.forEach(function(child) {
+      console.log(Object.keys(child.val()).length);
+      if(count == 0){
+        html_tab = html_tab+'<tr>';
+        for(var j = 0 ; j<Object.keys(child.val()).length ; j++ )
+        {
+           html_tab = html_tab+'<th>'+Object.keys(child.val())[j]+'</th>';
+        }
+        html_tab = html_tab+'</tr>';
+      }
+      else{
+         for(var j = 0 ; j<Object.keys(child.val()).length ; j++ )
+      var valor = child.child(Object.keys(child.val())[j]).val();
+        {
+           html_tab = html_tab+'<th>'+valor+'</th>';
+        }
+     }
+
+    count++;
+
+   });
+   html = html + html_tab;
+//   html = html+'</tr>';
+   html = html+'</table>';
+   html = html+'';
+
+   html = html+'';
+            
+   html = html+'       </table>';
+   html = html+'     </div>';
+   html = html+'   </div>';
+   html = html+' </div>';
+
+ document.getElementById('listaNodos').insertAdjacentHTML('beforeend', html);
+
+});
+
+
+
+}
--- upload_csv/web-start/subir_archivo.php
+++ upload_csv/web-start/subir_archivo.php
@@ -1,18 +1,55 @@
+<html lang="en">
+  <meta charset="utf-8">
+  <!-- Material Design Lite -->
+  <link rel="stylesheet" href="/upload_csv/web-start/styles/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/upload_csv/web-start/styles/bootstrap/js/bootstrap.min.js">
+  <script src="scripts/main.js"></script>
+  <script src="scripts/d3.js"></script>
+  <script src="scripts/d3.min.js"></script>
+ <script src="https://cdn.firebase.com/js/client/2.0.2/firebase.js"></script>
+
+
+</head>
+<body>
+<div class="container">
+
+
 <?php
-$target_dir = "/upload_csv/web-start/json/";
+$nombre = basename($_FILES["fileToUpload"]["name"]);
+$target_dir = "C:/xampp/htdocs/upload_csv/web-start/json/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 echo $target_file;
-exit();
+
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 // Allow certain file formats
 if($fileType != "csv" ) {
-    echo "Sorry, only CSV files are allowed. ";
+    echo "Solo archivos csv. ";
 }
 else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "El archivo ". basename( $_FILES["fileToUpload"]["name"]). " ha sido subido.";
+
+        echo '
+<input type="hidden" value="'.$_POST['nomb_tabla'].'" id="nomb_tabla">
+<input type="hidden" value = "'.$nombre.'" id="archivo">
+<script type="text/javascript">
+    convertir_csv();
+</script>
+  
+        ';
+
+
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Error al subir archivo.";
     }
+   }
 ?>
+
+</div>
+</body>
+</html>
+
+
+
+
