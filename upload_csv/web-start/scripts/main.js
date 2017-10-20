d2789f0c53c4b1d8a1fdbcd2d5d7ce9b7a360cf0
mrcm15 <mariarenee.canqui@gmail.com>
2017-10-19 20:12:27 -0400

    modificaciones

--- upload_csv/web-start/scripts/main.js
+++ upload_csv/web-start/scripts/main.js
@@ -25,104 +25,93 @@ function convertir_csv(){ //convertir csv a json y subir informacion a firebase
 
 function obtener_nodos(){
 
-  var html='';
- var messagesRef = new Firebase("https://pruebas-csv.firebaseio.com/"); 
-/*var rootRef = messagesRef.database.ref();
-var urlRef = rootRef.child("user1/DAA notes/URL");
-urlRef.once("value", function(snapshot) {
-  snapshot.forEach(function(child) {
-    console.log(child.key+": "+child.val());
-  });
-});
- */
-
- var t = '';
-
-  /*messagesRef.on('child_added', function(snapshot) {
-
-   // all records after the last continue to invoke this function
-   console.log(snapshot.key());
-   html = html+'<div class="card"> <div class="card-header" role="tab" id="headingOne"><h5 class="mb-0">';
-   html = html+'       <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">';
-   html = html+         snapshot.key();
-   html = html+'       </a> </h5></div>';
-
-   html = html+'   <div id="collapseOne" class="collapse " role="tabpanel" aria-labelledby="headingOne">';
-   html = html+'     <div class="card-block">';
-   html = html+'        <table>';
-            
-   html = html+'       </table>';
-   html = html+'     </div>';
-   html = html+'   </div>';
-   html = html+' </div>';
-            
-          
-
-   //, snapshot.val()
+   var html='';
+   var messagesRef = new Firebase("https://pruebas-csv.firebaseio.com/"); 
 
 
+  messagesRef.on('child_added',function(snap) {
+    
+  html='';
+  html = html+'<div class="card"> <div class="card-header" role="tab" id="headingOne"><h5 class="mb-0">';
+  html = html+'       <a data-toggle="collapse" data-parent="#accordion" href="#'+snap.key()+'" aria-expanded="true" aria-controls="collapseOne">';
+  html = html+         snap.key();
+  html = html+'       </a> </h5></div>';
 
-});
-console.log(t);*/
+  html = html+'   <div id="'+snap.key()+'" class="collapse " role="tabpanel" aria-labelledby="headingOne">';
+  html = html+'     <div class="card-block">';
+  html = html+'<div class="table-responsive" >';
+  html = html+'        <table class="table table-bordered">';
 
-//child('/scenes/' + projId)
-var keys = [];
-var counts = [];
-var ref = messagesRef.ref();
-messagesRef.on('child_added',function(snap) {
-  
-html='';
-html = html+'<div class="card"> <div class="card-header" role="tab" id="headingOne"><h5 class="mb-0">';
-   html = html+'       <a data-toggle="collapse" data-parent="#accordion" href="#'+snap.key()+'" aria-expanded="true" aria-controls="collapseOne">';
-   html = html+         snap.key();
-   html = html+'       </a> </h5></div>';
+  count = 0;
+  html_tab='';
+  snap.forEach(function(child) {
 
-   html = html+'   <div id="'+snap.key()+'" class="collapse " role="tabpanel" aria-labelledby="headingOne">';
-   html = html+'     <div class="card-block">';
-   html = html+'        <table>';
-  
 
-  //html = html+'<tr>';
-   
-   count = 0;
-   html_tab='';
-   snap.forEach(function(child) {
-      console.log(Object.keys(child.val()).length);
-      if(count == 0){
-        html_tab = html_tab+'<tr>';
-        for(var j = 0 ; j<Object.keys(child.val()).length ; j++ )
-        {
+ 
+    html_tab = html_tab+'<tr> ';
+    if(count == 0){
+      html_tab = html_tab+'<th>id</th>';
+      for(var j = 0 ; j<Object.keys(child.val()).length ; j++ )
+      {
            html_tab = html_tab+'<th>'+Object.keys(child.val())[j]+'</th>';
-        }
-        html_tab = html_tab+'</tr>';
       }
-      else{
-         for(var j = 0 ; j<Object.keys(child.val()).length ; j++ )
-      var valor = child.child(Object.keys(child.val())[j]).val();
-        {
-           html_tab = html_tab+'<th>'+valor+'</th>';
-        }
-     }
+      count ++;
+    }
+    html_tab = html_tab+'</tr>';
+    html_tab = html_tab+'<tr onclick="modificar_registro(this);">';
+    html_tab = html_tab+'<td>'+child.key()+'</td>';
 
-    count++;
+    for(var j = 0 ; j<Object.keys(child.val()).length ; j++ )
+    {
+      var valor = child.child(Object.keys(child.val())[j]).val();
+      html_tab = html_tab+'<td>'+valor+'</td>';
+    }
+      html_tab = html_tab+'</tr>';
 
    });
    html = html + html_tab;
 //   html = html+'</tr>';
    html = html+'</table>';
-   html = html+'';
-
-   html = html+'';
-            
-   html = html+'       </table>';
+   html = html+'     </div>';
    html = html+'     </div>';
    html = html+'   </div>';
    html = html+' </div>';
 
- document.getElementById('listaNodos').insertAdjacentHTML('beforeend', html);
+   document.getElementById('listaNodos').insertAdjacentHTML('beforeend', html);
 
 });
 
 
+//db.ref("-Users/-KUanJA9egwmPsJCxXpv").update({ displayName: "New trainer" });
+
 
 }
+var input_sel = true;
+var row_sel = '';
+
+var row_active = [];
+function modificar_registro(row){
+  var col = row.getElementsByTagName("td");
+ // var t = col[i].innerHTML;
+  row_active[col[0].innerHTML] = true;
+  
+  console.log(col[0].innerHTML);
+  if( col[0].innerHTML !=row_sel  && row_active[col[0].innerHTML]){
+    input_sel = false;
+    
+    row_sel= col[0].innerHTML;
+
+    for (var i = 0 ; i< col.length; i++)
+    {
+      var node = document.createElement("input"); 
+      node.value = col[i].innerHTML;
+       col[i].innerHTML = '';
+      col[i].appendChild(node);
+    }
+  }
+ 
+  //var myJsonString = JSON.stringify(yourArray);
+ // console.log(id);
+}
+
+
